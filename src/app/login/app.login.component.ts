ef12478771e6d8628e101b5d4074227e51ec2e0c
Deepak <deepak_gurav22@yahoo.co.in>
2018-09-10 08:48:29 -0400

    Initial Commit
    Initial Commit

---
+++ src/app/login/app.login.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+@Component({
+  selector:'app-root',
+  templateUrl:'./app.login.component.html',
+  styleUrls:['./app.login.component.css']
+})
+export class LoginComponent{
+  title='Login {title}'
+}
